7c4bd630c4aa961fc04e929a9b65ddba3e29324d
Piotr Polesiuk <ppolesiuk@cs.uni.wroc.pl>
2017-03-23 11:31:45 +0100

    Implicit arguments

--- coqsrc/Common/MBinder.v
+++ coqsrc/Common/MBinder.v
@@ -5,8 +5,15 @@ Inductive inc (V : Set) : Set :=
 | VS : V → inc V
 .
 
+Arguments VZ [V].
+Arguments VS [V] _.
+
 Inductive inc2_h (V : Set) : Set :=
 | V2_arg    : inc2_h V
 | V2_resume : inc2_h V
 | V2S       : V → inc2_h V
 .
+
+Arguments V2_arg    [V].
+Arguments V2_resume [V].
+Arguments V2S       [V] _.
--- coqsrc/Explicit/MSyntax.v
+++ coqsrc/Explicit/MSyntax.v
@@ -21,6 +21,19 @@ Inductive typ (W : world) (TV : Set) : Set :=
 | t_app      : typ W TV → typ W TV → typ W TV
 .
 
+Arguments t_var      [W] [TV] _.
+Arguments t_base     [W] [TV] _.
+Arguments t_effect   [W] [TV] _.
+Arguments t_row_nil  [W] [TV].
+Arguments t_row_cons [W] [TV].
+Arguments t_arrow    [W] [TV].
+Arguments t_forall   [W] [TV] _ _.
+Arguments t_app      [W] [TV] _ _.
+
+Notation t_arrow' σ ε τ := (t_app (t_app (t_app t_arrow σ) ε) τ).
+Notation "'〈' x ';' .. ';' y '/' z '〉'" :=
+  (t_app (t_app t_row_cons x) .. (t_app (t_app t_row_cons y) z) .. ).
+
 Inductive expr (W : world) (TV : Set) (V : Set) : Type :=
 | e_value  : value W TV V → expr W TV V
 | e_let    : expr W TV V → expr W TV (inc V) → expr W TV V
@@ -45,3 +58,21 @@ with handler (W : world) (TV : Set) (V : Set) : Set → Type :=
     handler W TV V Op →
     handler W TV V (inc Op)
 .
+
+Arguments e_value  [W] [TV] [V] _.
+Arguments e_let    [W] [TV] [V] _ _.
+Arguments e_app    [W] [TV] [V] _ _.
+Arguments e_tapp   [W] [TV] [V] _ _.
+Arguments e_open   [W] [TV] [V] _ _.
+Arguments e_handle [W] [TV] [V] _ _ _ _.
+Arguments e_primop [W] [TV] [V] _ _.
+
+Arguments v_var    [W] [TV] [V] _.
+Arguments v_eff_op [W] [TV] [V] _ _.
+Arguments v_lam    [W] [TV] [V] _ _ _.
+Arguments v_tlam   [W] [TV] [V] _ _.
+
+Arguments h_return [W] [TV] [V] _.
+Arguments h_op     [W] [TV] [V] _ _.
+
+Coercion e_value : value >-> expr.
